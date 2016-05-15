485539e5d3eba4856179f20680151dfaf5ab4d2d
Heng-Yi Wu <henry40408@gmail.com>
2016-05-16 01:16:56 +0800

    temp commit

--- src/main/java/CompilerRunner.java
+++ src/main/java/CompilerRunner.java
@@ -1,3 +1,6 @@
 public class CompilerRunner {
-    // empty
+    static public void main(String[] args) {
+        Compiler compiler = new Compiler();
+        compiler.compileLine("INT AAA, BBB=8, CCC=2; AAA = BBB + CCC;");
+    }
 }
